7e2d912dfd308cfbaf5995a155b872500820c7ce
David <53861457+SerugaDavid@users.noreply.github.com>
2023-03-16 11:24:12 +0100

    Added scripts

---
+++ create_jetty_base.bat
@@ -0,0 +1,3 @@
+mkdir jetty-base
+cd ./jetty-base
+java -jar ..\jetty-home-10.0.13\start.jar --add-module=server,http,deploy,annotations
---
+++ run_jetty_server.bat
@@ -0,0 +1,2 @@
+cd ./jetty-base
+java -jar ../jetty-home-10.0.13/start.jar
