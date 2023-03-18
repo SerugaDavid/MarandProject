06e9e54f79b2bc0e57fada79029928f11ae7842c
David <53861457+SerugaDavid@users.noreply.github.com>
2023-03-18 19:36:47 +0100

    Changed method getFlights, Added getId and DurationToTime, Minor changes

--- converter/Converter.java
+++ converter/Converter.java
@@ -10,21 +10,24 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
+import java.sql.Time;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Scanner;
 
 public class Converter {
+
     public static void main(String[] args) {
         String url = "http://localhost:8080/api/flights";
+        String path = "C:\\Users\\david\\Documents\\GitHub\\MarandProject\\converter\\data_file.csv";
         try {
-            JSONArray airlines = getNewAirlines("./data_file.csv");
+            JSONArray airlines = getNewAirlines(path);
             APIPost(airlines, url + "/airlines");
 
-            JSONArray airports = getNewAirports("./data_file.csv");
+            JSONArray airports = getNewAirports(path);
             APIPost(airports, url + "/airports");
 
-            JSONArray flights = getFlights("./data_file.csv");
+            JSONArray flights = getFlights(path, url);
             APIPost(flights, url);
 
         } catch (FileNotFoundException e) {
@@ -80,8 +83,103 @@ public class Converter {
         }
     }
 
-    public static JSONArray getFlights(String path) throws FileNotFoundException {
-        return null;
+    public static JSONArray getFlights(String path, String url) throws FileNotFoundException {
+        // TODO: call API to get airport id-s
+        // TODO: call API to get airline id-s
+
+        // read file
+        Scanner sc = new Scanner(new File(path));
+        String line;
+        String[] lineSplit;
+        JSONArray flights = new JSONArray();
+        JSONObject row;
+
+        while (sc.hasNextLine()) {
+            line = sc.nextLine().trim();
+            lineSplit = line.split("\\^");
+            row = new JSONObject();
+            row.put("FlightNumber", lineSplit[0]);
+            row.put("OriginAirportID", getId("airports", "abbreviation", lineSplit[1], url));
+            row.put("DestinationAirportID", getId("airports", "abbreviation", lineSplit[2], url));
+            row.put("Airlines_idAirlines", getId("airlines", "name", lineSplit[3], url));
+            row.put("Price", Double.parseDouble(lineSplit[4]));
+            row.put("Day", lineSplit[5]);
+            row.put("Time", lineSplit[6] + ":00");
+            row.put("Duration", durationToTime(lineSplit[7]));
+            row.put("AvailableSeats", Integer.parseInt(lineSplit[8]));
+            flights.put(row);
+        }
+
+        return flights;
+    }
+
+    /**
+     * Converts duration to time value
+     * @param duration duration of a flight in format 00h00m
+     * @return String value of time
+     */
+    public static String durationToTime(String duration) {
+        String hour = "00";
+        String min = "00";
+        if (duration.contains("h")) {
+            String[] split = duration.split("h");
+            hour = split[0];
+            duration = split[1];
+        }
+        min = duration.substring(0, duration.length()-1);
+        return hour + ":" + min + ":00";
+    }
+
+    /**
+     * Gets id of an airline or an airport for a given name or abbreviation
+     * @param table airline or airport
+     * @param column name or abbreviation
+     * @param parameter value for our column
+     * @param url url of our api
+     * @return id for given parameters
+     */
+    public static int getId(String table, String column, String parameter, String url) {
+        HttpURLConnection connection = null;
+        try {
+            connection = (HttpURLConnection) new URL(url + "/" + table + "/" + column + "/" + parameter).openConnection();
+        } catch (IOException e) {
+            System.out.println("Malformed URL!");
+            e.printStackTrace();
+            System.exit(1);
+        }
+        try {
+            connection.setRequestMethod("GET");
+        } catch (ProtocolException e) {
+            System.out.println("Protocol exception!");
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        try {
+            if (connection.getResponseCode() != 200) {
+                System.out.println("Connection failed");
+                System.exit(1);
+            }
+        } catch (IOException e) {
+            System.out.println("Connection failed");
+            System.exit(1);
+        }
+
+        String response = "";
+        try {
+            Scanner sc = new Scanner(connection.getInputStream());
+            while (sc.hasNextLine()) {
+                response += sc.nextLine() + "\n";
+            }
+            sc.close();
+        } catch (IOException e) {
+            System.out.println("Failed to create InputStream");
+            System.exit(1);
+        }
+
+        JSONArray entry = new JSONArray(response);
+
+        return entry.getJSONObject(0).getInt("idA" + column.substring(1));
     }
 
     /**
