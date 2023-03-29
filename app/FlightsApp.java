fffa019e8b7ba6ae289938997d39973b168c30cd
David <53861457+SerugaDavid@users.noreply.github.com>
2023-03-29 20:25:42 +0200

    Get and show flights.

--- app/FlightsApp.java
+++ app/FlightsApp.java
@@ -85,6 +85,98 @@ public class FlightsApp {
     private JSONArray getAirportsArray() {
         String url = this.url + "/airports";
 
+        return connect(url);
+    }
+
+    private JSONArray getAirlinesArray() {
+        String url = this.url + "/airlines";
+
+        return connect(url);
+    }
+
+    private JSONArray getFlights(String origin, String destination, JSONArray airports) {
+        int o = 0;
+        int d = 0;
+
+        for (int i = 0; i < airports.length(); i++) {
+            if (airports.getJSONObject(i).getString("Name").equals(origin)) {
+                o = airports.getJSONObject(i).getInt("idAirports");
+            }
+            if (airports.getJSONObject(i).getString("Name").equals(destination)) {
+                d = airports.getJSONObject(i).getInt("idAirports");
+            }
+        }
+
+        String url = this.url + "/flights/origin-destination/" + o + "/" + d;
+        return connect(url);
+    }
+
+    private void updateTable(JSONArray flights, JSONArray airports, JSONArray airlines) {
+        if (flights.length() == 0) {
+            System.out.println("No flights found");
+            return;
+        }
+
+        Vector<String> columns = getColumns(flights);
+        Vector<Vector<String>> data = getData(flights, airports, airlines);
+
+        this.flightsTable.setModel(new DefaultTableModel(data, columns));
+    }
+    
+    private Vector<Vector<String>> getData(JSONArray flights, JSONArray airports, JSONArray airlines) {
+        Vector<Vector<String>> data = new Vector<>();
+        for (int i = 0; i < flights.length(); i++) {
+            Vector<String> row = new Vector<>();
+            JSONObject flight = flights.getJSONObject(i);
+
+            row.add(flight.getString("FlightNumber"));
+            row.add(getAirportName(flight.getInt("OriginAirportID"), airports));
+            row.add(getAirportName(flight.getInt("DestinationAirportID"), airports));
+            row.add(getAirlineName(flight.getInt("Airlines_idAirlines"), airlines));
+            row.add(flight.getString("Price"));
+            row.add(flight.getString("Day"));
+            row.add(flight.getString("Time"));
+            row.add(flight.getString("Duration"));
+            row.add(flight.getString("AvailableSeats"));
+
+            data.add(row);
+        }
+        return data;
+    }
+
+    private String getAirportName(int id, JSONArray airports) {
+        for (int i = 0; i < airports.length(); i++) {
+            if (airports.getJSONObject(i).getInt("idAirports") == id) {
+                return airports.getJSONObject(i).getString("Name");
+            }
+        }
+        return "";
+    }
+
+    private String getAirlineName(int id, JSONArray airlines) {
+        for (int i = 0; i < airlines.length(); i++) {
+            if (airlines.getJSONObject(i).getInt("idAirlines") == id) {
+                return airlines.getJSONObject(i).getString("Name");
+            }
+        }
+        return "";
+    }
+
+    private Vector<String> getColumns(JSONArray flights) {
+        Vector<String> columns = new Vector<>();
+        columns.add("Številka leta");
+        columns.add("Odhodno letališče");
+        columns.add("Prihodno letališče");
+        columns.add("Letalska družba");
+        columns.add("Cena");
+        columns.add("Dan");
+        columns.add("Čas odhoda");
+        columns.add("Dolžina leta");
+        columns.add("Prosti sedeži");
+        return columns;
+    }
+
+    private JSONArray connect(String url) {
         HttpURLConnection connection = null;
         try {
             connection = (HttpURLConnection) new URL(url).openConnection();
